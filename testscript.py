419eebcb7e363d7084328b70c9199b759028bc4a
Alex Gregorie <gregoriedev@gmail.com>
2017-10-24 01:02:58 -0400

    Cleaned up script to remove duplicate spaces

--- testscript.py
+++ testscript.py
@@ -5,7 +5,15 @@ words = []
 email = """
 Dear Hiring Manager,
 
-I would like to express my deep interest in a position as editorial assistant for your publishing company.As a recent graduate with writing, editing, and administrative experience, I believe I am a strong candidate for a position at the 123 Publishing Company.You specify that you are looking for someone with strong writing skills. As an English major at XYZ University, a writing tutor, and an editorial intern for both a government magazine and a college marketing office, I have become a skilled writer with a variety of publication experience.My maturity, practical experience, attention to detail, and eagerness to enter the publishing business will make me an excellent editorial assistant. I would love to begin my career with your company, and am confident that I would be a beneficial addition to the 123 Publishing Company.I have enclosed my resume, and will call within the next week to see if we might arrange a time to speak together.
+I would like to express my deep interest in a position as editorial assistant for your publishing company.
+
+As a recent graduate with writing, editing, and administrative experience, I believe I am a strong candidate for a position at the 123 Publishing Company.
+
+You specify that you are looking for someone with strong writing skills. As an English major at XYZ University, a writing tutor, and an editorial intern for both a government magazine and a college marketing office, I have become a skilled writer with a variety of publication experience.
+
+My maturity, practical experience, attention to detail, and eagerness to enter the publishing business will make me an excellent editorial assistant. I would love to begin my career with your company, and am confident that I would be a beneficial addition to the 123 Publishing Company.
+
+I have enclosed my resume, and will call within the next week to see if we might arrange a time to speak together.
 
 Thank you so much for your time and consideration.
 
@@ -13,6 +21,8 @@ Sincerely,
 
 Jane Jones
 """
+#End email entry
+
 # Clean up the email body by removing line breaks
 email = email.replace('\n',' ')
 
@@ -21,7 +31,9 @@ sentences = email.split('.')
 
 for each in sentences:
     sentence = each.strip()
+    sentence = sentence.replace("  "," ")
     sentences_list.append(sentence)
+
 #Break each sentence into words
 
 for each in sentences_list:
